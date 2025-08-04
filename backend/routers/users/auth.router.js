ffdcd2d99cd602858862d795beac3040dc47fe72
jeel narola <narolajeel29@gmail.com>
2025-08-04 12:18:45 +0530

    Login Logic

--- backend/routers/users/auth.router.js
+++ backend/routers/users/auth.router.js
@@ -1,5 +1,7 @@
 import express from 'express'
-import { register } from '../../controllers/auth/auth.controller.js'
+import { login, register } from '../../controllers/auth/auth.controller.js'
 export const authRoute = express.Router()
 
 authRoute.post('/register',register)
+authRoute.post('/login',login)
+authRoute.post('/logout',login)
